54b48fdec17c7d1b9c6c2dfb934b1740f8014c7b
Eric Lehmann <ericlehmann88@gmail.com>
2020-11-30 12:33:00 +0100

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID: '336392039878-tqls42ecln1nnaihljipppp11c84ku6d.apps.googleusercontent.com',
-    googleClientSecret: 'wXA3QksAi0BoKM8e0b7xv08s',
-    mongoURI: 'mongodb+srv://eric_admin:Aihh3742@!Aihh@cluster0.iqmlp.mongodb.net/emaily?retryWrites=true&w=majority',
-    cookieKey: 'redacted'
-}
-
