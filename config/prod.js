a1eb213c9688657912d9f7ffbd561a767d5fb443
Eric Lehmann <ericlehmann88@gmail.com>
2020-12-05 02:15:18 +0100

    'fix2'

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,7 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    facebookClientID: '2868429433390855',
+    facebookClientSecret: 'f9b8b3dbfcc8ccea783eb856ab26a60a',
 }
