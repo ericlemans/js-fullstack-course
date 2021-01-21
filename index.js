3ae0434e3429522754f0a28b4a3746a260b2c3cf
Eric Lehmann <ericlehmann88@gmail.com>
2021-01-21 17:23:03 -0300

    fix stripe key

--- index.js
+++ index.js
@@ -59,18 +59,16 @@ app.get('/test', (req, res) => {
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets like our main.js file or main.css file
+    app.use(express.static('client/build'))
+
+    // Express will serve up the index.html file if it doesn't recognize the route
     const path = require('path');
-
-    // serve production assets e.g. main.js if route exists
-    app.use(express.static(path.resolve(__dirname, "../client/build")));
-
-    // serve index.html if route is not recognized
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-    });
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
 }
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
